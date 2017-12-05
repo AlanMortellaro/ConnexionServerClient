f47559984084abc7d1a15acd99eacd0936851acd
AlanMortellaro <alan.mortellaro@hotmail.com>
2017-12-05 17:26:33 +0100

    add error gestion in server.py if connection not enabled

--- server.py
+++ server.py
@@ -1,20 +1,26 @@
 import socket
 import os
 
-hote = ''
-port = 25565
-main_co = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-main_co.bind((hote, port))
-main_co.listen(5)
-client_co , info = main_co.accept()
-msg = b''
-while msg != b'stop':
-    msg = input('MOI => ' + str(info[0]) + " > ")
-    msg = msg.encode()
-    client_co.send(msg)
-    recv = client_co.recv(1024)
-    recv = recv.decode()
-    print(recv)
-print("Console > Session Closed")
-client_co.close()
-main_co.close()
+try:
+    hote = ''
+    port = 25565
+    main_co = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    main_co.bind((hote, port))
+    main_co.listen(5)
+    client_co , info = main_co.accept()
+    msg = b''
+    while msg != b'stop':
+        msg = input('MOI => ' + str(info[0]) + " > ")
+        msg = msg.encode()
+        client_co.send(msg)
+        recv = client_co.recv(1024)
+        recv = recv.decode()
+        print(recv)
+    print("Console > Session Closed")
+    client_co.close()
+    main_co.close()
+except:
+    print("\n   --------------------------------------------------------------")
+    print("  |                     Open client session                      |")
+    print("   --------------------------------------------------------------\n")
+    os.system("pause")
